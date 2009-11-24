0fc13591516e4df3b00c79415e8cb998d7bb1648
Imanol Celaya <ilcra1989@gmail.com>
2009-11-24 21:59:24 +0100

    added license among other things

--- os/kernel.c
+++ os/kernel.c
@@ -1,7 +1,9 @@
 #include "vga.h"
+#include "cpuid.h"
 
 void kmain( void* mbd, unsigned int magic )
 {
+    char cpu[48];
     /*specify intel syntax for asm*/
     asm(".intel_syntax noprefix\n");
     
@@ -16,7 +18,11 @@ void kmain( void* mbd, unsigned int magic )
     char * boot_loader_name =(char*) ((long*)mbd)[16];
     
     /* Kernel starts here */
-    /*kprint(boot_loader_name);*/
+    
+    cpu_model(cpu);
+    
+    mv_cursor(1, 0);
+    kprint(cpu);
     mv_cursor(2, 0);
     kprint(boot_loader_name);
     mv_cursor(3, 0);
